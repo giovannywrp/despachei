d0667e539cd8d5c3c5080ecd5119e8fb0e4337ed
Giovanny Willian <giovannywrp@hotmail.com>
2016-11-03 00:28:45 -0200

    adicionando script para menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
+window.addEventListener('scroll', handleMenu());
+
 function handleScroll(target){
   $('html, body').animate({
       scrollTop: $(target).offset().top
   }, 1200);
+}
+
+function handleMenu(){
+  var header = document.getElementById('headerContainer');
+  if(window.pageYOffset > 1 ){
+    if(header.className !== 'topMenu fixedMenu'){
+      header.className += ' fixedMenu';
+    }
+  }
+  if(window.pageYOffset < 2 ){
+    header.className = 'topMenu';
+  }
 }
