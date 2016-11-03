d358eac54e810b20e29845e27a74f570c96cd216
Giovanny Willian <giovannywrp@hotmail.com>
2016-11-03 13:18:01 -0200

    alterando responsividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,4 @@
-window.onload = handleMenu();
-window.addEventListener('scroll', handleMenu());
-
-function handleScroll(target){
-  $('html, body').animate({
-      scrollTop: $(target).offset().top
-  }, 1200);
-}
-
-function handleMenu() {
+document.onload = function () {
   var header = document.getElementById('headerContainer');
   if(window.pageYOffset > 1 ){
     if(header.className !== 'topMenu fixedMenu'){
@@ -17,4 +8,22 @@ function handleMenu() {
   if(window.pageYOffset < 2 ){
     header.className = 'topMenu';
   }
+}
+
+window.addEventListener('scroll', function () {
+  var header = document.getElementById('headerContainer');
+  if(window.pageYOffset > 1 ){
+    if(header.className !== 'topMenu fixedMenu'){
+      header.className += ' fixedMenu';
+    }
+  }
+  if(window.pageYOffset < 2 ){
+    header.className = 'topMenu';
+  }
+});
+
+function handleScroll(target){
+  $('html, body').animate({
+      scrollTop: $(target).offset().top
+  }, 1200);
 }
