4aeb83999fcb3f1210ca8464b8cd9ddd0facb1c5
Giovanny Willian <giovannywrp@hotmail.com>
2016-11-03 11:02:15 -0200

    adição dos media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,15 @@ function handleScroll(target){
       scrollTop: $(target).offset().top
   }, 1200);
 }
+
+document.onload = function () {
+  var header = document.getElementById('headerContainer');
+  if(window.pageYOffset > 1 ){
+    if(header.className !== 'topMenu fixedMenu'){
+      header.className += ' fixedMenu';
+    }
+  }
+  if(window.pageYOffset < 2 ){
+    header.className = 'topMenu';
+  }
+}
